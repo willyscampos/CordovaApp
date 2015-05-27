8cc3d68f4e81f2a74f2537732722581d1116b8fc
willyscampos <willyscampos@gmail.com>
2015-05-27 15:01:36 -0300

    Primeiro Código
    Primeiro Código

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('myapp', [
+  'ngRoute'
+]);
+
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', { templateUrl: 'pages/home.html' });
+    $routeProvider.when('/home', { templateUrl: 'pages/home.html'});
+    $routeProvider.when('/list', { templateUrl: 'list.html'});
+    $routeProvider.when('/detail/:itemId', { templateUrl: 'detail.html'});
+    $routeProvider.when('/settings', { templateUrl: 'settings.html' });
+});
+
+/* Controllers */
+app.controller('MainCtrl', function ($rootScope, $scope) {
+    $scope.title = "Start";
+});
+
+app.controller('HomeCtrl', function ($rootScope, $scope) {
+    $scope.title = "Start";
+});
